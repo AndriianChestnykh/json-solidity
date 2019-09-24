ee2f59e03af972d0db60bce6905d287bc4af3ac3
Andriian Chestnykh <achestnykh@provectus.com>
2019-09-24 16:01:25 +0300

    Update example.js

--- example.js
+++ example.js
@@ -4,8 +4,9 @@ const fs = require('fs');
 const crypto = require('crypto');
 
 const jsData = JSON.stringify({
-  someKey1: 'someValue1',
-  someKey2: 'someValue2',
+  keyToChange: 'change me !',
+  keyNotToTouch: 'do not touch me !',
+  keyToDelete: 'delete me !',
 });
 
 const artifacts = JSON.parse(fs.readFileSync('./contracts/build/Storage.json'));
@@ -14,6 +15,7 @@ const artifacts = JSON.parse(fs.readFileSync('./contracts/build/Storage.json'));
 const web3 = new Web3('http://127.0.0.1:7545');
 const storage = new web3.eth.Contract(artifacts.abi, artifacts.networks['5777'].address);
 
+// Smart contracts should be deployed befohand with 'truffle migrate'
 (async function() {
   const address = (await web3.eth.getAccounts())[0];
   const id = '0x' + crypto.randomBytes(32).toString('hex');
@@ -21,10 +23,13 @@ const storage = new web3.eth.Contract(artifacts.abi, artifacts.networks['5777'].
   await storage.methods.set(id, ...jsonToEth(jsData), address)
     .send({ from: address, gas: 1000000 }).catch(error => console.log(error.message));
 
-  await storage.methods.setByDataKey(id, Web3.utils.stringToHex('newKey'), Web3.utils.toHex('someValueOfNewKey'))
+  await storage.methods.setByDataKey(id, Web3.utils.stringToHex('keyToChange'), Web3.utils.toHex('changed !'))
     .send({ from: address, gas: 1000000 }).catch(error => console.log(error.message));
 
-  await storage.methods.setByDataKey(id, Web3.utils.stringToHex('someKey1'), Web3.utils.toHex('someNewKey1Value'))
+  await storage.methods.removeByDataKey(id, Web3.utils.stringToHex('keyToDelete'))
+    .send({ from: address, gas: 1000000 }).catch(error => console.log(error.message));
+
+  await storage.methods.setByDataKey(id, Web3.utils.stringToHex('newKey'), Web3.utils.toHex('I am a new key !'))
     .send({ from: address, gas: 1000000 }).catch(error => console.log(error.message));
 
   const ethDataFromContract = await storage.methods.get(id).call();
