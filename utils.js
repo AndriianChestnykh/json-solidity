e2549e39306138e2e76947dd7f8dba241bf7a897
Andriian Chestnykh <achestnykh@provectus.com>
2019-09-24 15:38:52 +0300

    Fix utils

--- utils.js
+++ utils.js
@@ -64,7 +64,7 @@ function parseJson(data) {
 function parseEth({keys, values, offsets}) {
     if (!(keys instanceof Array)) throw new Error('keys is not instanceof Array');
     if (values !== null && typeof values !== 'string') throw new Error('values is not null and is not topeof string');
-    if (values.indexOf(HEX_PREFIX) === -1) throw new Error('Values is not hex prefixed');
+    if (typeof values === 'string' && values.indexOf(HEX_PREFIX) === -1) throw new Error('Values is not hex prefixed');
     if (!(offsets instanceof Array)) throw new Error('offsets is not instanceof Array');
     if (offsets.length !== keys.length) throw new Error('keys and offsets lengths are not equal');
 
